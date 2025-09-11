b2ea148e62ecd48145acfedcddd4a78a6e194050
spfarm <spfarm@contoso.local>
2025-09-11 14:05:48 +0000

    adding demo webpart

---
+++ SPFX/demoWebPart/src/webparts/demoWebPart/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IDemoWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'DemoWebPartWebPartStrings' {
+  const strings: IDemoWebPartWebPartStrings;
+  export = strings;
+}
